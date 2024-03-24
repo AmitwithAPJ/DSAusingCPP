056b61bde023c1010fbf156a329b213bbd09ffcb
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-24 12:50:09 +0530

    Add files via upload

---
+++ #LB_005_array_creation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10];
+    int size=10;
+    for(int i=0;i<size;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+    
+    return 0;
+}
