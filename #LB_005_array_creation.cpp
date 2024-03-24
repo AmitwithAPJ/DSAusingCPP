42f4d166b6f752a02f15aca56668203de0921412
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-24 12:55:21 +0530

    Update #LB_005_array_creation.cpp

--- #LB_005_array_creation.cpp
+++ #LB_005_array_creation.cpp
@@ -1,12 +1,13 @@
+//How to create a array
 #include <iostream>
 using namespace std;
 int main()
 {
-    int arr[10];
-    int size=10;
-    for(int i=0;i<size;i++)
+    int arr[10]; //array creation of size 10
+    int size=10; 
+    for(int i=0;i<size;i++)   
     {
-        cout << arr[i] << " ";
+        cout << arr[i] << " ";   //accessing the array through indexing
     }
     
     
