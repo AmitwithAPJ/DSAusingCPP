c503b853689d1873e267eb85fcbd3dd9ea8b4adc
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-25 21:13:29 +0530

    Update #LB_006_array_doubling.cpp

--- #LB_006_array_doubling.cpp
+++ #LB_006_array_doubling.cpp
@@ -1,18 +1,19 @@
+//Program to Doubling the element in the array
 #include <iostream>
 using namespace std;
 int main ()
 {
-    int arr[10];
+    int arr[10]; //array created of size 10
     int size=10;
     for (int i=0;i<size;i++)
     {
         cout << "enter the elements: " ;
-        cin >> arr[i];
+        cin >> arr[i];   //taking input for the created array
     }
     for (int i=0; i<size;i++)
     {
-        arr[i]*=2;
-        cout << arr[i] << " ";
+        arr[i]*=2;         // doubling of element in an array through indexes
+        cout << arr[i] << " ";  //printing of each element after doubling it
     }
     
     
