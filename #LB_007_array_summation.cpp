6fd0c86f26bc59605de64828711630c10c9d9c96
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-25 21:16:47 +0530

    Update #LB_007_array_summation.cpp

--- #LB_007_array_summation.cpp
+++ #LB_007_array_summation.cpp
@@ -1,18 +1,19 @@
+//Program for Summation of element in an array
 #include <iostream>
 using namespace std;
 int main ()
 {
-    int arr[10];
+    int arr[10];  //array of size 10 created
     int size=10;
     for (int i=0;i<size;i++)
     {
         cout << "enter the elements: " ;
-        cin >> arr[i];
+        cin >> arr[i];   //taking the inputs (index wise)
     }
     for (int i=0; i<size;i++)
     {
-        int sum=0;
-        sum=sum+arr[i];
+        int sum=0;        //sum variable created to store the values after each summation
+        sum=sum+arr[i];   //summation of element through indexes
     }
     cout << "The Summation of array elements are: " << sum;
     
