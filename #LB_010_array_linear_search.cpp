36786076a405b7997b166b3bdc1a8440994b8df7
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-26 21:55:42 +0530

    Add files via upload

---
+++ #LB_010_array_linear_search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int arr[6]={2,4,6,7,10,12};
+    int size=6;
+    int target=8;
+    int i=0;
+    bool flag=0;
+    int j=6;
+    while(i<=j)
+    {
+        if(arr[i]==target||arr[j]==target)
+        {
+            flag=1;
+            break;
+        }
+        i++;
+        j--;
+    }
+    if (flag==1)
+    {
+        cout << "Target Found!!";
+    }
+    else 
+    {
+        cout << "Target Not Found!!";
+    }
+    
+    return 0;
+}
