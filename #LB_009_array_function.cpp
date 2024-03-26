3ece80e5d4d6f4bad79d43633f02a5ed349a755a
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-26 21:54:07 +0530

    Update #LB_009_array_function.cpp

--- #LB_009_array_function.cpp
+++ #LB_009_array_function.cpp
@@ -1,17 +1,18 @@
+//program to use array in function
 #include <iostream>
 using namespace std;
-void basic_array(int arr[], int size)
+void basic_array(int arr[], int size)  //basic_array function created which will take input in an array
 {
     for(int i=0;i<size;i++)
     {
         cout << "Enter the Element: ";
-        cin >> arr[i];
+        cin >> arr[i];             //taking input through indexes
     }
 }
 int main ()
 {
-    int arr[5];
+    int arr[5];    //array created of size 5
     int size=5;
-    basic_array(arr,size);
+    basic_array(arr,size);      //array and size of array is passed to the function (basic_array)
     return 0;
 }
