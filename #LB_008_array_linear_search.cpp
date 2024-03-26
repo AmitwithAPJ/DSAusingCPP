0fe49acc10a4c17e00e08622c26bba75eaa9b6bd
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-26 21:51:05 +0530

    Update #LB_008_array_linear_search.cpp

--- #LB_008_array_linear_search.cpp
+++ #LB_008_array_linear_search.cpp
@@ -1,17 +1,18 @@
+//program to find a target in an array through linear seach
 #include<iostream>
 using namespace std;
 int main ()
 {
     int arr[]={2,4,6,8,10,12};
     int size=6;
-    int target=8;
-    bool flag=0;
+    int target=8;   //target to find in the array
+    bool flag=0;    //flag variable created to check whether target is found or not 
     for(int i=0; i<size; i++)
     {
         if(arr[i]==target)
         {
             break;
-            flag=1;
+            flag=1;   //flag value set to 1 if target found(however, 1 means true)
         }
     }
     if(flag==1)
