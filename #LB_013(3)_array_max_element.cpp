9a32419a5af4ae8a049d009cbcdfa361b56bbd65
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-29 13:37:24 +0530

    Add files via upload

---
+++ #LB_013(3)_array_max_element.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <limits.h>
+int main ()
+{
+    int arr[10]={1,2,3,4,5,-1,100,-100,400,321};
+    int size=10;
+    int ans=arr[0];
+    for (int i=1; i<size; i++)
+    {
+      ans=max(ans,arr[i]);
+    }
+    cout << "The Maximum is: " << ans;
+    
+    
+    
+    
+    
+    
+    return 0;
+}
---
+++ #LB_014(3)_array_min_element.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <limits.h>
+int main ()
+{
+    int arr[10]={1,2,3,4,5,1,100,100,400,321};
+    int size=10;
+    int ans=INT_MAX;
+    for (int i=0; i<size; i++)
+    {
+      if(arr[i]<ans)
+        ans=arr[i];
+    }
+    cout << "The Maximum is: " << ans;
+    
+    
+    
+    
+    
+    
+    return 0;
+}
