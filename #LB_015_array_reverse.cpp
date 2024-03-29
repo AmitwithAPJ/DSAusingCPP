e9081f6458257ba5a6efec06a148ddba52fb58a1
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-29 13:38:21 +0530

    Add files via upload

---
+++ #LB_015_array_reverse.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int arr[7]={2,4,6,8,10,0,12};
+    int size=6;
+    for (int i=0;i<size;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "Updating!!" << endl;
+    for (int i=0;i<size/2;i++)
+    {
+        swap(arr[i],arr[size-i-1]);
+    }
+    cout << "Answer finded!!" << endl;
+    for (int i=0;i<size;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+    
+    
+    return 0;
+}
