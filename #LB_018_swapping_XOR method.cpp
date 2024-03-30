6fcd4baf30dc25523fc76d0c361a15abe79b5928
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-30 19:22:57 +0530

    Add files via upload

---
+++ #LB_018_swapping_XOR method.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void swapping(int arr[], int index1, int index2)
+{
+    arr[index1]=(arr[index1]^arr[index2]);
+    arr[index2]=arr[index1]^arr[index2];
+    arr[index1]=arr[index1]^arr[index2];
+}
+int main ()
+{
+   int size=5;
+   int arr[5]={1,2,3,4,5};
+   swapping(arr,1,3);
+   for(int i=0; i<size; i++)
+   {
+       cout << arr[i] << " ";
+   }
+    
+    
+    
+    
+    return 0;
+}
