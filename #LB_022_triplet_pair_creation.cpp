ac67d10c48633fbd33210c657f69c3673189b0b2
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-31 11:30:14 +0530

    Add files via upload

---
+++ #LB_022_triplet_pair_creation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void triplet_printing(int arr[], int size)
+{
+    int sum=0;
+    for (int i=0;i<size;i++)
+    {
+        for(int j=0; j<size;j++)
+        {
+            for(int k=0;k<size;k++)
+            {
+                cout << "(" << arr[i] << ", " << arr[j] << ", " << arr[k] << ")";
+                sum=arr[i]+arr[j]+arr[k];
+                cout << "- Pair Sum is: " << sum << endl;
+            }
+        }
+    }
+}
+int main ()
+{
+    int arr[]={10,20,30};
+    int size=3;
+    triplet_printing(arr,size);
+    
+
+    return 0;
+}
