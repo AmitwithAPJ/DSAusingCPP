81c04a222648c5da128848f2f34c26cfcc122700
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-03-31 11:29:52 +0530

    Add files via upload

---
+++ #LB_021_pair_creation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void pair_printing(int arr[],int size)
+{
+    int sum=0;
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            cout << "(" << arr[i] << ", " << arr[j] << ")";
+            sum=arr[i]+arr[j];
+            cout << "- Pair Sum is: " << sum << endl;
+        }
+    }
+}
+int main()
+{
+    int arr[]={10,20,30};
+    int size=3;
+    pair_printing(arr,size);
+    return 0;
+}
