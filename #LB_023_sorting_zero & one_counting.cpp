f4698b6ec4c3f22b485b9660ef310c39b43f8850
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-01 09:38:08 +0530

    Add files via upload

---
+++ #LB_023_sorting_zero & one_counting.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+void sorting_array(int arr[], int size)
+{
+    int zero_count=0;
+    int one_count=0;
+    int brr[9]={};
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i]==0)
+        {
+            zero_count++;
+        }
+        else if (arr[i]==1)
+        {
+            one_count++;
+        }
+    }
+    for(int i=0;i<zero_count;i++)
+    {
+        arr[i]=0;
+    }
+    for(int i=zero_count;i<size;i++)
+    {
+       arr[i]=1;
+    }
+}
+int main ()
+{
+  int arr[]={1,0,0,1,1,0,0,0,1};
+  int size=9;
+  sorting_array(arr,size);
+  for(int i=0;i<size;i++)
+  {
+      cout << arr[i] << " ";
+  }
+    
+    return 0;
+}
