2e788e37be3093f47ee25ed0795da659e7f7f1f6
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-03 18:52:40 +0530

    Update #LB_011_array_counting(0&1).cpp

--- #LB_011_array_counting(0&1).cpp
+++ #LB_011_array_counting(0&1).cpp
@@ -7,9 +7,13 @@ void counting(int arr[], int size)
     for (int i=0;i<size;i++)
     {
         if(arr[i]==0)
+        {
            zero_count++;
+        }   
         else if(arr[i]==1)
+        {
            one_count++;
+        }   
     }
     cout << "The One's are: " <<  one_count << endl;
     cout << "The Zero's are: " << zero_count;
