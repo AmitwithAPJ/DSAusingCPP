19ad1f37244695f4cc47f0d23a8a90d8557f8891
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-03 07:15:53 +0530

    Add files via upload

---
+++ #LB_031_sorting_0_1_using_Dutch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+//sorting zeros and ones in an array!!
+void sorting_array(int arr[], int size)
+{
+    int left=0;
+    int right=size-1;
+    while(left<=right) //equal wale case me error aa raha pata nhi kyu fix it!!!
+    {
+        if(arr[left]==0)
+        {
+           left++;
+        }
+        if(arr[left]==1)
+        {
+             swap(arr[left],arr[right]);
+            right--;
+        }
+    }
+}
+int main()
+{
+    int arr[]={0,1,1,0,0,1,0,1,0,0,1};
+    int size=11;
+    sorting_array(arr,size);
+    for(int i=0;i<size;i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
