99db3e15ec596883b31bc63dcf8ed157e8b9e7f6
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-03 07:15:17 +0530

    Add files via upload

---
+++ #LB_029_shifting_array_right_by_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void shifting_array(int arr[],int size)
+{
+    int temp1=arr[size-1];
+    int temp2=arr[size-2];
+    for(int i=size-1;i>1;i--)
+    {
+        arr[i]=arr[i-2];
+    }
+    arr[0]=temp1;
+    arr[1]=temp2;
+}
+int main ()
+{
+ int arr[]={10,20,30,40,50,60};
+ int size=6;
+ shifting_array(arr,size);
+ for(int i=0;i<size;i++)
+ {
+     cout << arr[i] << " ";
+ }
+    
+    
+    
+    
+    return 0;
+}
