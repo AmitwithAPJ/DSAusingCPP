740c01dbe426143551d13180bc84d3386db0669c
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-03 07:15:35 +0530

    Add files via upload

---
+++ #LB_030_DutchNationalFlag.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void dutchNationalFlag(int arr[], int size) {
+    int low = 0, mid = 0, high = size - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        } else if (arr[mid] == 1) {
+            mid++;
+        } else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    int arr[]={0,1,2,0,2,1,0,1,1,0,0,1,2,1,2,0,1,2};
+    int size=18;
+
+    dutchNationalFlag(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
