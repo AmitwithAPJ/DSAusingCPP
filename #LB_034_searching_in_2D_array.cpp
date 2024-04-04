51c9187e62fea002d8e8d3f8a05232ab54ac4e81
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-04 19:05:01 +0530

    Add files via upload

---
+++ #LB_034_searching_in_2D_array.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+bool searching_2D(int arr[][4],int rows,int cols, int target)
+{
+    bool flag=false;
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<cols;j++)
+        {
+            if(arr[i][j]==target)
+            {
+                flag=true;
+                break;
+            }
+        }
+    }
+    return flag;
+}
+int main()
+{
+    int rows=2;
+    int cols=2;
+    int target=9;
+    int arr[][4]={
+                  {0,1},
+                  {4,5},
+                 };
+    if(searching_2D(arr,rows,cols,target))
+    {
+        cout << "Target Found!!";
+    }
+    else
+    {
+        cout << "Target not Found!!";
+    }
+    return 0;
+}
