826ce7ac7b44043147d8df18c24df0b8003da64d
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-04 19:04:13 +0530

    Add files via upload

---
+++ #LB_032_2D_array_basics_matrix.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+void D2_array(int arr[][4], int rows, int cols)
+{
+    for (int i=0;i<rows;i++)
+    {
+        for(int j=0;j<cols;j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+int arr[][4]={
+              {2,3,4,5},
+              {1,0,4,2},
+              {9,8,7,6},
+              {0,1,2,3}
+            };
+int rows=4;
+int cols=4;
+cout << "traversing the array!!" << endl;
+D2_array(arr,rows,cols);
+cout << "traversing ended!!" << endl;
+    return 0;
+}
