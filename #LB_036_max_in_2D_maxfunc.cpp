674019399554155653c7cb80f028641525b6f746
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-05 06:53:11 +0530

    Add files via upload

---
+++ #LB_036_max_in_2D_maxfunc.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int max_2D(int arr[][3],int rows, int cols)
+{
+    int temp=arr[0][0];
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<cols;j++)
+        {
+            if(arr[i][j]>temp)
+            {
+                temp=arr[i][j];
+            }
+        }
+    }
+    return temp;
+}
+int main()
+{
+int arr[][3]={
+              {2,3,4},
+              {10,-2,30},
+             };
+int rows = sizeof(arr) / sizeof(arr[0]); // Calculate number of rows
+int cols = sizeof(arr[0]) / sizeof(arr[0][0]);
+int result=max_2D(arr,rows,cols);
+cout << "The Max element is: " << result << endl;
+    return 0;
+}
