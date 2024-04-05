739eed4e44e37e142a68a2e01b9f6cace52d5263
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-05 19:04:57 +0530

    Update #LB_037_max_in_2D_maxfunc.cpp

--- #LB_037_max_in_2D_maxfunc.cpp
+++ #LB_037_max_in_2D_maxfunc.cpp
@@ -1,6 +1,7 @@
+// Program to find max element in 2D array!!
 #include <iostream>
 using namespace std;
-int max_2D(int arr[][5],int rows, int cols)
+int max_2D(int arr[][5],int rows, int cols) //max_2D function created to find max element
 {
     int ans=arr[0][0];
     for (int i=0;i<rows;i++)
@@ -19,9 +20,9 @@ int main()
                   {8,10,23,45,76},
                   {0,-2,34,56,90},
                  };
-int rows=sizeof(arr)/sizeof(arr[0]);
+int rows=sizeof(arr)/sizeof(arr[0]);   
 int cols=sizeof(arr[0])/sizeof(arr[0][0]);
-int result=max_2D(arr,rows,cols);
+int result=max_2D(arr,rows,cols);   //array, rows & cols are paased as argument in the max_2D function
 cout << "The Max element is: " << result;
     return 0;
 }
