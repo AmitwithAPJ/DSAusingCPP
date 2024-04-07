db1bfdc1b6d75911e135961c234686b975a6fa0d
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-07 23:56:19 +0530

    Add files via upload

---
+++ #LB_042_sum_counter_diagonal_2Darray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int summation_2D_diagonal(int arr[][5], int rows, int cols)
+{
+    int sum_diagonal=0;
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=cols-(i+1);j<cols-i;j++)
+        {
+            sum_diagonal += arr[i][j];
+        }
+    }
+    return sum_diagonal;
+}
+int main()
+{
+    int arr[][5]={
+                  {2,3,4,5,6},
+                  {0,1,2,5,9},
+                  {-2,5,9,0,3},
+                  {9,8,7,6,9},
+                  {100,200,300,400,500},
+                 };
+int rows=sizeof(arr)/sizeof(arr[0]);
+int cols=sizeof(arr[0])/sizeof(arr[0][0]);
+int result=summation_2D_diagonal(arr,rows,cols);
+cout << "The Diagonal Element summation is: " << result;
+    return 0;
+}
