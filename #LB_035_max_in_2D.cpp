7ba496f9524fb980f7a5f5c4e9bd76024ce8af75
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-08 19:29:30 +0530

    Update #LB_035_max_in_2D.cpp

--- #LB_035_max_in_2D.cpp
+++ #LB_035_max_in_2D.cpp
@@ -1,9 +1,10 @@
+//Finding the maximum element in an array!!
 #include <iostream>
-#include <limits.h>
+#include <limits.h>   //Header file for INT_MIN
 using namespace std;
-int max_2D(int arr[][4],int rows, int cols)
+int max_2D(int arr[][4],int rows, int cols)  //max_2D function to find MAX Element!!
 {
-    int ans=INT_MIN;
+    int ans=INT_MIN;  //storing INT_MIN in the ans to compare the other elemnents in an array!!
     for(int i=0;i<rows;i++)
     {
         for(int j=0;j<cols;j++)
@@ -14,19 +15,19 @@ int max_2D(int arr[][4],int rows, int cols)
             }
         }
     }
-    return ans;
+    return ans;    /returning the final maximum number 
 }
 int main()
 {
 int result;
 int rows=4;
 int cols=4;
-int arr[][4]={
+int arr[][4]={                                //2D Array created 
               {2,3,4,5},
               {12,13,14,15},
               {22,23,24,25}, 
               {32,33,34,35},
-             };
+             }; 
 result=max_2D(arr,rows,cols);
 cout << "The Max result is: " << result;
 return 0;    
