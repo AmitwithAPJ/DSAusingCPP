b5e4751afb937ea37399477e82f4c4712180a97d
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-09 07:03:44 +0530

    Add files via upload

---
+++ #LB_047_sorting_LC(Red_White_Blue).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+void sorting_array(int arr[], int size)
+{
+   int left=0;
+   int mid=0;
+   int right=size-1;
+   while(mid<right)
+   {
+        if(arr[mid]==0)
+         {
+            swap(arr[left],arr[mid]);
+            left++;
+            mid++;
+         }
+         else if(arr[mid]==1)
+         {
+            mid++;
+         }
+         else if(arr[mid]==2)
+         {
+             swap(arr[mid],arr[right]);
+             right--;
+         }
+   }
+}
+int main()
+{
+    int arr[]={2,0,0,1,0,1,0,2,1,2,0,1,2,1,0,0};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    sorting_array(arr,size);
+    for(int i=0;i<size;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+    
+    return 0;
+}
