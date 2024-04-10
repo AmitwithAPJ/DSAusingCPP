a88d2e046e3376b7a3b08e5089a5e73ad4373883
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-10 09:31:16 +0530

    Add files via upload

---
+++ #LB_051_Missing_element_array_traversing.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+void missing_element(int arr[], int size, int total_num)
+{
+    for(int i=0;i<total_num;i++)
+    {
+        bool flag=false;
+        for(int j=0;j<size;j++)
+        {
+            if(i==arr[j])
+            {
+                flag=true;
+                break;
+            }
+        }
+        if(flag==false)
+        {
+            cout << "The Missing Element is: " << i << endl;
+        }
+    }
+}
+int main ()
+{
+ int arr[]={2,0,1,4,5,6,7};   
+ int size=sizeof(arr)/sizeof(arr[0]);
+ int total_num=7;
+ missing_element(arr,size,total_num);  
+    
+    
+    return 0;
+}
