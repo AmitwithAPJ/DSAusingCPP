3744998561b9f7af431337fd13a368a30e239466
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-10 09:31:39 +0530

    Add files via upload

---
+++ #LB_052_Missing_element_array_summation.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void missing_element(int arr[], int size, int total_nums)
+{
+    int temp=arr[0];
+    for(int i=1;i<size;i++)
+    {
+        temp=(temp^arr[i]);
+    }   
+    for(int j=0;j<=total_nums;j++)
+    {
+        temp=(temp^j);
+    }
+    cout << "The Missing element is: " << temp;
+}
+int main()
+{
+   int arr[]={2,0,1,4,5,6,7};   
+ int size=sizeof(arr)/sizeof(arr[0]);
+ int total_nums=7;
+ missing_element(arr,size,total_nums); 
+    
+    
+    
+    return 0;
+}
