072c7dc52a612e29b76fd11036153f47f9cdcccd
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-10 09:30:42 +0530

    Add files via upload

---
+++ #LB_050_shifting_array_M2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void shifiting(int arr[], int size)
+{
+    vector<int>brr;
+    brr.push_back(arr[size-1]);
+    brr.push_back(arr[size-2]);
+    for(int i=0;i<size-2;i++)
+    {
+        brr.push_back(arr[i]);
+    }
+    for(int i=0;i<brr.size();i++)
+    {
+        arr[i]=brr[i];
+        cout << arr[i] << " ";
+    }
+}
+int main ()
+{
+ int arr[]={10,20,30,40,50,60};
+ int size=sizeof(arr)/sizeof(arr[0]);
+ shifiting(arr,size);
+    
+    
+
+    return 0;
+}
