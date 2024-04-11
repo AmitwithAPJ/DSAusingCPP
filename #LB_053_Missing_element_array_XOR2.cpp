3c8917c99d5acfb113d1b246d31017bce3bbd66e
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-11 18:36:27 +0530

    Add files via upload

---
+++ #LB_053_Missing_element_array_XOR2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void missing_element(int arr[], int size, int total_nums)
+{
+    int sum_array=0;
+    int total_sums=0;
+    for(int i=0;i<size;i++)
+    {
+        sum_array=sum_array+arr[i];
+    }
+    for(int i=0;i<=total_nums;i++)
+    {
+       total_sums=total_sums+i; 
+    }
+    cout << "The Missing Element is: " << total_sums-sum_array;
+}
+int main()
+{
+int arr[]={2,0,1,4,5,6,7};
+int total_nums=7;
+int size=sizeof(arr)/sizeof(arr[0]);
+missing_element(arr,size,total_nums);
+    
+    
+    
+    
+    return 0;
+}
