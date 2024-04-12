f55b686eab22f9699a9fd5557c562b520430e582
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-12 19:19:11 +0530

    Add files via upload

---
+++ #LB_057_last_Occurance.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int first_occurance(int arr[], int size, int target)
+{
+    int start=0;
+    int end=size-1;
+    int mid;
+    int temp_ans=-1;
+    while(start<=end)
+    {
+        mid=(start+end)/2;
+        if(target==arr[mid])
+        {
+           temp_ans=mid;
+           start=mid+1;
+        }
+        else if(target<arr[mid])
+        {
+            end=mid-1;
+        }
+        else if(target>arr[mid])
+        {
+            start=mid+1;
+        }
+    }
+    return temp_ans;
+}
+int main ()
+{
+    int arr[]={10,20,22,23,24,30,40,50};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int target=30;
+    int result=first_occurance(arr,size,target);
+    if(result!=-1)
+    {
+    cout << "The First Occurance of " << target << " is: " << result;
+    }
+    else 
+    {
+        cout << target << " isn't founded!!!";
+    }
+    return 0;
+}
