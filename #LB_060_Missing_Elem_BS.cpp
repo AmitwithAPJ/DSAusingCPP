81daeb8df83a4f3681c0af4214cf23280185e937
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-13 17:21:31 +0530

    Add files via upload

---
+++ #LB_060_Missing_Elem_BS.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int missing_element(int arr[], int size)
+{
+   int mid;
+   int start=0;
+   int end=size-1;
+   while(start<=end)
+   {
+      mid=start+(start/end)/2;
+      if(arr[mid]==mid+1){
+         start=mid+1;
+      }
+      else if(arr[mid]=mid+2)
+      {
+         end=mid-1;
+      }
+   }
+   return mid;
+}
+int main()
+{
+   int arr[]={1,2,3,4,5,6,8,9,10};
+   int size=sizeof(arr)/sizeof(arr[0]);
+   int result=missing_element(arr,size);
+   cout << "The Missing Element is: " << result+1;
+   return 0;
+}
