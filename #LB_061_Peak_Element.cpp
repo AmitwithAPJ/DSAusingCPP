d4f7e481660f7c8fd431c6478131793a50cf2139
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-13 17:22:38 +0530

    Add files via upload

---
+++ #LB_061_Peak_Element.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int Peak_Element(int arr[], int size)
+{
+    int start=0;
+    int end=size-1;
+    int ans=0;
+    while(start<end)
+    {
+        int mid=start+(end-start)/2;
+        if(arr[mid]>=arr[mid-1])
+        {
+            start=mid+1;
+            ans=mid;
+        }
+        else if(arr[mid]>arr[mid+1])
+        {
+            end=mid-1;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    int arr[]={10,20,30,60,50,40};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int result= Peak_Element(arr,size);
+    cout << "The Peak Element is: " << arr[result] << endl;
+
+
+
+
+    return 0;
+}
