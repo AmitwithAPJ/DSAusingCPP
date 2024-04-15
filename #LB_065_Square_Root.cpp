2127dc606d46697771e28b32612f2b7d210462ac
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-15 13:42:04 +0530

    Add files via upload

---
+++ #LB_065_Square_Root.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+double square_root(int number)
+{
+    double start=0;
+    double ans=-1;
+    double end=number;
+    double precision=0.001; // very important aspect;
+    while(end-start>precision)
+    {
+        double mid=start+(end-start)/2;
+        if(mid*mid==number)
+        {
+            
+            return mid;
+        }
+        else if(mid*mid>number)
+        {
+            end=mid;
+        }
+        else if(mid*mid<number)
+        {
+            ans=mid;
+            start=mid;
+        }
+    }
+    //ans=static_cast<int>(ans*1000+0.5)/1000.0;
+    return ans;
+    
+}
+int main()
+{
+    int number;
+    cout << "Enter The Number of Which You Want o Find Square Root: ";
+    cin >> number;
+    double result=square_root(number);
+    cout << "The Square root of " << number  << " is: " << fixed << setprecision(3) << result;
+
+
+
+    return 0;
+}
