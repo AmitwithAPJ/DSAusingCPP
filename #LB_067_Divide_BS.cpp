197580aa9e50783c3d1e8b1ff699d132cfeba8be
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-16 12:45:52 +0530

    Add files via upload

---
+++ #LB_067_Divide_BS.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+double Divide_BS(int Dividend, int Divisor)
+{
+    double start=0;
+    double ans=-1;
+    double end=Dividend;
+    double precision=0.01;
+    while(end-start>precision)
+    {
+        double mid=start+(end-start)/2;
+        if(mid*Divisor==Dividend)
+        {
+            return mid;
+        }
+        else if(mid*Divisor>Dividend)
+        {
+            end=mid;
+        }
+        else
+        {
+            ans=mid;
+            start=mid;
+        }
+    }
+    ans=int(ans*100+0.5)/100.0;
+    return ans;
+
+}
+int main()
+{
+    int Dividend=29;
+    int Divisor=7;
+    double result=Divide_BS(Dividend,Divisor);
+    //cout << "The Answer is: " << fixed << setprecision(2) << result;
+    cout << "The Answer is: " << result;
+
+
+
+
+    return 0;
+}
