7f79be69399c16e35a71c7aa6e68161b4d3f9915
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-16 12:46:28 +0530

    Add files via upload

---
+++ #LB_069__nearlySorted_BS.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int nearlysorted_BS(int arr[], int size, int target)
+{
+    int start=0;
+    int ans=-1;
+    int end=size-1;
+    while(start<=end)
+    {
+        int mid=start+(end-start)/2;
+        if(arr[mid]==target)
+        {
+            return mid;
+        }
+        else if(arr[mid-1]==target)
+        {
+            return mid-1;
+        }
+        else if(arr[mid+1]==target)
+        {
+            return mid+1;
+        }
+        else if(target>arr[mid])
+        {
+            start=mid+2;
+        }
+        else
+        {
+            end=mid-2;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int arr[]={20,10,40,30,60,50,70,80};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int target=0;
+    int result=nearlysorted_BS(arr,size,target);
+    if((result!=-1))
+    {
+    cout << "The Target Found at: " << result;
+    }
+    else 
+    {
+        cout << "Target Not Found!!";
+    }
+
+
+    return 0;
+}
