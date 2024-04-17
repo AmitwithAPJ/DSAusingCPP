2859a50179f2995fe647fdb46b1cfbefb4174da5
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-17 12:39:40 +0530

    Add files via upload

---
+++ #LB_072_Bubble_Sort_M2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+void Bubble_sort_M2(vector<int>&arr)
+{
+    int start=0;
+    int end=arr.size()-1;
+     while(start<=end) 
+     {
+        if(arr[start]>=arr[end])
+        {
+            swap(arr[start],arr[end]);
+            start++;
+            end--;
+        }
+        else if(arr[start]<arr[end])
+        {
+            end--;
+        }
+     }
+}
+
+int main()
+{
+    vector<int>arr={2,3,-2,0,1,8};
+    Bubble_sort_M2(arr);
+    for(int i=0;i<arr.size();i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+
+    return 0;
+}
