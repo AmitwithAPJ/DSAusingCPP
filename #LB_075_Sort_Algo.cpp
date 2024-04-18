37f2d7e41f5f7d70a4a238c813d9994e31445a2b
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-18 18:07:35 +0530

    Add files via upload

---
+++ #LB_075_Sort_Algo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm> //for sorting 
+using namespace std;
+#include <vector>
+void printing(vector<int>arr)
+{
+    for(int i=0;i<arr.size();i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+bool mycomp(int &a, int &b)
+{
+    return a>b;
+}
+int main()
+{
+vector<int>arr={55,33,22,11,44};
+sort(arr.begin(),arr.end(),mycomp);
+printing(arr);
+return 0;
+}
