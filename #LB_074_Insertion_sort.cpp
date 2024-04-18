08402fefc8bb5f4f656816b9ada35051f98fa90a
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-18 18:06:56 +0530

    Add files via upload

---
+++ #LB_074_Insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void insertion_sort(vector<int>&arr)
+{
+    for(int i=1;i<arr.size();i++)
+    {
+        int temp=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>temp)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=temp;
+    }
+}
+int main()
+{
+    vector<int>arr={5,4,3,2,1};
+    insertion_sort(arr);
+    for(int i=0;i<arr.size();i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+
+
+    return 0;
+}
