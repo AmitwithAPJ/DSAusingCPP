9ac01e7c6b5356c9ddcb1f0014143a35bf80028e
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-20 06:09:55 +0530

    Add files via upload

---
+++ #LB_080_Null_Value_Char.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char ch[25]={'Amit Patel'};
+ // cout << "Enter the string: ";
+  //cin.getline(ch,25);
+  int i=0;
+  int temp;
+  while(ch[i]!='\0')
+  {
+     temp=i;
+     i++;
+  }
+  int value=static_cast<int>(temp+1);
+  cout << "The Value of Null characetr is: " << value << endl;
+  
+
+
+
+
+  return 0;
+}
