b1b7d6a008be4caf8ca8785d5a28e92ee7d872b8
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-21 20:20:23 +0530

    Add files via upload

---
+++ #LB_084_Reversing_string_InbuiltF.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm> // Include the algorithm header
+#include <string> // normal string header file
+//#include <cstring> for M2 Method jo i c-stye string method hai
+using namespace std;
+int main()
+{
+  char ch[20];
+  int size=sizeof(ch)/sizeof(ch[0]);
+  cin.getline(ch,size);
+  cout << "Before Swapping: " << ch << endl;
+  string str(ch);
+  reverse(str.begin(),str.end());
+  cout << "Inbuilt Func: " << str << endl;
+  //reverse(ch,ch+strlen(ch));
+  //cout << "Inbuilt FuncM2: " << ch << endl;
+
+
+  return 0;
+}
