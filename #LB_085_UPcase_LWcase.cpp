6fa6ad746edfa07e5bdee2f53db15b44fa22ef6e
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-21 20:20:45 +0530

    Add files via upload

---
+++ #LB_085_UPcase_LWcase.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+  char ch[25];
+  cin.getline(ch,25);
+  int size=strlen(ch);
+  for(int i=0;i<size;i++)
+  {
+      if(ch[i]>='A' && ch[i]<='Z')
+      {
+        int temp=static_cast<int>(ch[i]-'A'+'a');
+        //int temp=static_cast<int>(ch[i])+32;
+        cout << static_cast<char>(temp); 
+      }
+      else if(ch[i] !=' ')
+      {
+          cout << ch[i];
+      }
+      else
+      {
+          cout << " ";
+      }
+  }
+
+  return 0;
+}
