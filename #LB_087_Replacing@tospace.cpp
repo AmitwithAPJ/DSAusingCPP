f2d654864f7963e3c8bad2d6a4a1d708bc740f5f
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-23 06:54:27 +0530

    Add files via upload

---
+++ #LB_087_Replacing@tospace.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void replacing(char ch[], int size)
+{
+   int i=0;
+   while(ch[i]!='\0')
+   {
+     if(ch[i]=='@')
+     {
+      cout << " ";
+     }
+     else
+     {
+       cout << ch[i];
+     }
+     i++;
+   }
+}
+int main()
+{
+  char ch[25];
+  int size=sizeof(ch)/sizeof(ch[0]);
+  cout << "Enter the string: ";
+  cin.getline(ch,size);
+  replacing(ch,size);
+}
