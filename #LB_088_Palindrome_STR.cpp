b5a84fc321c41e99477299cfa4deb69e04fa6d10
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-23 06:54:44 +0530

    Add files via upload

---
+++ #LB_088_Palindrome_STR.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+#include<string.h>
+bool checking(char ch[])
+{
+   int start=0;
+   int end=strlen(ch)-1;
+   while(start<=end)
+   {
+      if(ch[start]==ch[end])
+      {
+        start++;
+        end--;
+      }
+      else
+      {
+        return false;
+      }
+   }
+   return true;
+}
+int main()
+{
+  char ch[25];
+  cout << "Enter the String to Check Palindrome: ";
+  cin.getline(ch,25);
+  if(checking(ch))
+  {
+    cout << "It is a Palindrome!!" << endl;
+  }
+  else
+  {
+    cout << "It is not a Palindrome!!" << endl;
+  }
+
+  return 0;
+}
