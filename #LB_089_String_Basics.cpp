525265570f52354a514fa738b860dc9c9894a433
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-24 21:35:35 +0530

    Add files via upload

---
+++ #LB_089_String_Basics.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main()
+{
+  string name;
+    //basic name printing in string!!
+ // cin >> name;
+ //cout << "printing: " << name;
+    //name printing through loop for each index!!
+ /*int i=0;
+  while(name[i]!='\0')
+  {
+    cout << "At Index: " << i << " Character: " << name[i] << endl;
+    i++;
+  }
+  */
+    //checking Null Characters!!
+ int i=0;
+ getline(cin,name);
+ while(i>=0)
+ {
+  if(name[i]=='\0')
+  {
+     cout << "Null Character Founded at: " << i << " ASCII Value: " 
+     << static_cast<int>(name[i]);
+     break;
+  }
+  i++;
+ }
+
+
+
+
+
+  return 0;
+}
