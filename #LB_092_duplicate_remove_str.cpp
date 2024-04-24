a1a85516ffd2e59fa2e08944d438492395362f28
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-24 21:36:41 +0530

    Add files via upload

---
+++ #LB_092_duplicate_remove_str.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+void removing_duplicates(string str)
+{
+   int index=0;
+   string temp;
+   while(index<str.length())
+   {
+    if(temp.length()>0 && temp[temp.length()-1]==str[index])
+    {
+       temp.pop_back();
+    }
+    else
+   {
+      temp.push_back(str[index]);
+   }
+   index++;
+   }
+   cout << "The String is: " << temp << endl;
+}
+int main()
+{
+  string str;
+  cout << "Enter the string: ";
+  getline(cin,str);
+  removing_duplicates(str);
+
+
+
+  return 0;
+}
