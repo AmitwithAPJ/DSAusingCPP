391d5af66890ff0dcd72451982bec646c3d33165
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-26 05:56:35 +0530

    Add files via upload

---
+++ #LB_098_Cipher.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+string decoding(string key, string message)
+{
+  char temp='a';
+  char arr[200]={0};
+  for(auto ch: key)
+  {
+     if(ch!=' ' && arr[ch]==0)
+     {
+        arr[ch]=temp;
+        temp++;
+     }
+  }
+  string ans;
+  for(auto ch: message)
+  {
+     if(ch!=' ')
+     {
+        char temp1=arr[ch];
+        ans.push_back(temp1);
+     }
+     else
+     {
+       ans.push_back(' ');
+     }
+  }
+  return ans;
+}
+int main()
+{
+   string key="Theek Hai Bhai";
+   string message="Ha BaT";
+   string result=decoding(key,message);
+   cout << result;
+
+
+
+
+
+   return 0;
+}
