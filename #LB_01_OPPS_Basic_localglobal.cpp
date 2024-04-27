10e3bbde8e6e769e9bb14922c829cb235304d644
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-27 06:36:45 +0530

    Add files via upload

---
+++ #LB_01_OPPS_Basic_localglobal.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int x=5;  //global variable 
+int main()
+{
+    cout << x <<endl;
+    int x=10;        //local varaible created despite the name is same 
+    cout << x << endl;
+    {
+       int x=20;        //another local variable created 
+        cout << x << endl;
+    }
+    cout << x << endl;
+    cout << ::x<< endl; //accessing global variable 
+    
+    
+    
+    
+    
+    return 0;
+}
+   
