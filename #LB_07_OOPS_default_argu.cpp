ca56e084c0d397f7a72abf5275a234aa410f5017
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-04-30 14:59:24 +0530

    Add files via upload

---
+++ #LB_07_OOPS_default_argu.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+class Abc
+{
+  private:
+   int x;
+   int *y;
+   int z;
+  public:
+   Abc(int _x, int _y, int _z=0)
+   {
+      x=_x;
+      y=new int(_y);
+      z=_z;
+   } 
+   int getX() const
+   {
+      return x;
+   }
+   void setX(int _value)
+   {
+      x=_value;
+   }
+   int getY() const
+   {
+      return *y;
+   }
+   void setY(int _value)
+   {
+     *y=_value;
+   }
+};
+void printabc(const Abc &a)
+{
+    cout << a.getX() << endl;
+    cout << a.getY() << endl;
+}
+int main()
+{
+    Abc a(1,2);
+    printabc(a);
+    return 0;
+}    
