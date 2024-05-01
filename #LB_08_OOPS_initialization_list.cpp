af1b8cc9ad356508c88998bc9b543c075ea89e53
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-01 14:57:33 +0530

    Add files via upload

---
+++ #LB_08_OOPS_initialization_list.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class Abc
+{
+    private:
+     int x;
+     int *y;
+     const int z=10;
+    public:
+     Abc(int _x, int _y, int _z=10): x(_x), y(new int(_y)), z(_z)
+     { 
+        //x=_x; //can be done by it will failed for z as it is const
+       cout << "Using the Initailization list!!" << endl;
+      // z=10;    //cannot do that bcoz of const
+      cout << z << endl;
+     }
+     int setX()
+     {
+        return x;
+     }
+     ~Abc()
+     {
+         delete y;  //deleting the dynamic memory using destructor
+     }
+};
+
+int main()
+{
+    Abc a(1,2,3);
+    cout << a.setX() << endl;
+
+
+
+
+    return 0;
+}
