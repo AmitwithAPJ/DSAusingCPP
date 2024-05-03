c3f42790f908e4f6dcd1c186c229482bd6510c00
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-03 08:17:06 +0530

    Add files via upload

---
+++ #LB_15_OOPS_static_data_func.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class data_func
+{
+    public:
+     int x, y;
+     data_func(): x(0),y(0){}
+    static void printing()    //no instances passed to the function
+    {
+       cout << x << " " << y << endl; //means you can directly access the variables as 
+                                       //"this" pointer is applicable in static data function
+     }
+};
+int main()
+{
+    data_func D1;
+    data_func::printing();
+    return 0;
+}
+
+// correct to access
+   static void printing(const data_func &D1)
+{
+   cout << D1.x << " " << D1.y << endl;
+}
+//and
+// data function::printing(D1);
