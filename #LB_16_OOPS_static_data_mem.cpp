2b74a90c16f118622d68b2684d519a46b9bfc196
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-04 14:34:03 +0530

    Add files via upload

---
+++ #LB_16_OOPS_static_data_mem.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class data_mem
+{
+   public: 
+   static int x;
+    static int y;
+   data_mem(int _x, int _y)  //can't do initialiszation list here
+   {
+      x=_x;
+      y=_y;
+      cout << "Ctor Called!!" << endl;
+   }
+   void printing() const
+   {
+      cout << x << " " << y << endl;
+   }
+};
+void print(const data_mem &D1)
+{
+   cout << D1.x << " "<< D1.y << endl;
+}
+int data_mem::x;
+int data_mem::y;
+int main()
+{
+  data_mem D1(1,2);
+  D1.printing();
+  print(D1);
+  
+
+   return 0;
+}
