912842282f1078a956357e213bc3e397f4a238e8
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-04 14:34:39 +0530

    Add files via upload

---
+++ #LB_18_OOPS_data_func_M3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class data_func
+{
+   private:
+    int x, y;
+   public: 
+    data_func(int _x, int _y)
+    {
+       x=_x;
+       y=_y;
+    } 
+    static void printing(const data_func &D1)
+    {
+       cout << D1.x << " " << D1.y << endl;
+    }
+};
+int main()
+{
+   data_func D1(2,1);
+   data_func::printing(D1);
+
+
+
+
+
+   return 0;
+}
