7f1093bacdef51127f75df816aaf02a9543bd4a5
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-06 15:12:31 +0530

    Add files via upload

---
+++ #LB_22_OOps_single_inheri.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class Animal
+{
+   public:
+     int age;
+     int weight;
+     string color;
+     int legs;
+     bool tail;
+    void run()
+    {
+      cout << "They Can un fastly!!" << endl;
+    }
+    void eat()
+    {
+       cout << "They eat a lot!!" << endl;
+    }
+};
+class Dog:private Animal
+{ 
+   public:
+    Dog(int _age, int _weight, string _color, int _legs, bool _tail): 
+    Animal{_age, _weight, _color, _legs, _tail}
+    {
+     /* age=_age;
+      weight=_weight;
+      color=_color;
+      legs=_legs;
+      tail=_tail; */
+    }
+    void barking()
+    {
+      cout << "Dogs barks a lot!! " << endl;
+    }
+};
+int main()
+{
+  Dog D1(2,20,"Brown",4,1);
+  D1.barking();
+//cout << D1.color << endl;
+
+
+
+   return 0;
+}
