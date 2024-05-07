fe992eda7e5371df10d5e1421750b8fcc57c129d
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-07 10:58:53 +0530

    Update #LB_012_array_linear_search_funct.cpp

--- #LB_012_array_linear_search_funct.cpp
+++ #LB_012_array_linear_search_funct.cpp
@@ -7,22 +7,22 @@ int searching(int arr[], int size, int target)
     {
         if (arr[i]==target)
         {
-            flag=1;
+            flag=1;   //setting the value of flag as true!!
             break;
         }
     }
 
-    return flag;
+    return flag;    //returinng the vlaue of flag!!   
 }
 int main ()
 {
-    int arr[6]={3,4,-1,2,6,0};
+    int arr[6]={3,4,-1,2,6,0};   //array of size 6 created
     int size=6;
-    int target=-1;
- int bool_ans=searching(arr,size,target);
- if (bool_ans==1)
+    int target=-1;             //setting the target as 1
+ int bool_ans=searching(arr,size,target);    //passed the argument  in the bool_ans
+ if (bool_ans==1)             //checking the conditions thorugh "if" conditional statement
  {
-     cout << "Target Found!!";
+     cout << "Target Found!!";    
  }
  else
  {
