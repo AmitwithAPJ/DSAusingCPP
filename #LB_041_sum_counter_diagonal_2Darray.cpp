ad2955dde3d56168e327730770ba05da757e0374
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-08 14:54:59 +0530

    Update #LB_041_sum_counter_diagonal_2Darray.cpp

--- #LB_041_sum_counter_diagonal_2Darray.cpp
+++ #LB_041_sum_counter_diagonal_2Darray.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int summation_2D_diagonal(int arr[][5],int rows, int cols)
 {
-    int sum_diagonal=0;
+    int sum_diagonal=0;   //initializing the value!! 
     for(int i=0;i<rows;i++)
     {
         for(int j=0;j<cols;j++)
         {
-            if(i==j)
+            if(i==j)    //checking the condition
             {
-            sum_diagonal += arr[i][j];
+            sum_diagonal += arr[i][j];   //addition of diagonal element in sum_diagonal variable!!
             }
         }
     }
@@ -17,16 +17,16 @@ int summation_2D_diagonal(int arr[][5],int rows, int cols)
 }
 int main()
 {
-    int arr[][5]={
+    int arr[][5]={                  //created 2D array for the matrix 
                   {2,3,4,5,6},
                   {0,1,2,5,9},
                   {-2,5,9,0,3},
                   {9,8,7,6,9},
                   {100,200,300,400,500},
                  };
-int rows=sizeof(arr)/sizeof(arr[0]);
-int cols=sizeof(arr[0])/sizeof(arr[0][0]);
-int result=summation_2D_diagonal(arr,rows,cols);
-cout << "The Diagonal Element summation is: " << result;
+int rows=sizeof(arr)/sizeof(arr[0]);   //getting row size by sizeof fucntion!!
+int cols=sizeof(arr[0])/sizeof(arr[0][0]); //getting column size by sizeof function!!
+int result=summation_2D_diagonal(arr,rows,cols);   //passing arguments to the function!!
+cout << "The Diagonal Element summation is: " << result;  
     return 0;
 }
