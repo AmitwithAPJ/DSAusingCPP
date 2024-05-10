2fb5addf02c64b46e4edf5afa4c98e3544f74ad8
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-10 23:29:54 +0530

    Update #LB_045_sorting_nega_posi_M2.cpp

--- #LB_045_sorting_nega_posi_M2.cpp
+++ #LB_045_sorting_nega_posi_M2.cpp
@@ -6,11 +6,12 @@ void sorting(int arr[], int size)
     int right=size-1;
     while(left<right)
     {
+        //the conditions are very important for the program to avoid edges case error!!
         while(arr[left]<0 && left<right)
         {
             left++;
         }
-        while(arr[right]>0 && left<right)
+        while(arr[right]>0 && left<right)  //This is normal case to do sorting!!
         {
             right--;
         }
