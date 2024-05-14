0b55f905121f50f6718f92d0d4762262287d4019
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-15 01:41:15 +0530

    Update #LB_049_shifting_array.cpp

--- #LB_049_shifting_array.cpp
+++ #LB_049_shifting_array.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 using namespace std;
-void shifting_array(int arr[], int size)
+void shifting_array(int arr[], int size)  //shifting_array function used to do shifting of elements!!
 {
-    int temp1=arr[size-1];
-    int temp2=arr[size-2];
+    int temp1=arr[size-1];   //tenp1 variable stores array's initial element.
+    int temp2=arr[size-2];   //temp2 varaible stores the array second most initaial element.
     for(int i=size-1;i>1;i--)
     {
-        arr[i]=arr[i-2];
+        arr[i]=arr[i-2];   //updating the array elements to make the shift 
     }
-    arr[0]=temp2;
+    //updating the remaining cases
+    arr[0]=temp2;   
     arr[1]=temp1;
 }
 int main()
 {
-    int arr[]={10,20,30,40,50,60};
+    int arr[]={10,20,30,40,50,60};   //an array of size of 6 is created!!
     int size=sizeof(arr)/sizeof(arr[0]);
-    shifting_array(arr,size);
-    for(int i=0;i<size;i++)
+    shifting_array(arr,size);   //passing argument to the fucntion.
+    //printing the shifted array after the updation!!
+    for(int i=0;i<size;i++)  
     {
         cout << arr[i] << " ";
     }
     
     return 0;
 }
+//Note:- You Can use another function printing the element (Before & After)!!
