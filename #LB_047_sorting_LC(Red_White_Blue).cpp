5ccf9d82ca903e17130d1019e6e0efec9a932e46
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-15 01:35:29 +0530

    Update #LB_047_sorting_LC(Red_White_Blue).cpp

--- #LB_047_sorting_LC(Red_White_Blue).cpp
+++ #LB_047_sorting_LC(Red_White_Blue).cpp
@@ -7,11 +7,11 @@ void sorting_array(int arr[], int size)
    int right=size-1;
    while(mid<right)
    {
-        if(arr[mid]==0)
+        if(arr[mid]==0)  //the condition is for the mid which will be used in array!!
          {
             swap(arr[left],arr[mid]);
-            left++;
-            mid++;
+            left++;   //lweft incremented!!
+            mid++;    //mid incremented in case to avoid edge cases.
          }
          else if(arr[mid]==1)
          {
@@ -26,9 +26,9 @@ void sorting_array(int arr[], int size)
 }
 int main()
 {
-    int arr[]={2,0,0,1,0,1,0,2,1,2,0,1,2,1,0,0};
-    int size=sizeof(arr)/sizeof(arr[0]);
-    sorting_array(arr,size);
+    int arr[]={2,0,0,1,0,1,0,2,1,2,0,1,2,1,0,0};  //a long array created to check at vast level!!
+    int size=sizeof(arr)/sizeof(arr[0]);  //finding the size of array thought the sizeof fucntion.
+    sorting_array(arr,size);   //passing arguments to the sortibng_array function.
     for(int i=0;i<size;i++)
     {
         cout << arr[i] << " ";
