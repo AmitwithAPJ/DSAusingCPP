0655ec3de4ff30024e32ba944d8ec73d0a5ff82c
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-15 22:49:37 +0530

    Update #LB_024_sorting_zero & one_swapping.cpp

--- #LB_024_sorting_zero & one_swapping.cpp
+++ #LB_024_sorting_zero & one_swapping.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
-void sorting_pointers(int arr[], int size)
+void sorting_pointers(int arr[], int size)  //sorting_pointers function!!
 {
     for(int i=0,j=size-1;i<j;)
     { 
       if(arr[i]==0 && arr[j]==1)
       {
-          i++;
-          j--;
+          i++;   //this will increase the i's value!!
+          j--;   //this will decrease the j's value!!
       }
       else if(arr[i]==1 && arr[j]==0)
       {
-         swap(arr[i],arr[j]);
+         swap(arr[i],arr[j]);   //swap the element present at ith and jth position.
          i++;
-         j--;
+         j--;  //j will be drecement to shift the position.
       }
       else if(arr[i]==0 && arr[j]==0)
       {
@@ -27,9 +27,9 @@ void sorting_pointers(int arr[], int size)
 }
 int main ()
 {
-    int arr[]={0, 1, 0, 1, 0, 0, 1, 0, 1};
+    int arr[]={0, 1, 0, 1, 0, 0, 1, 0, 1};  //an array of size 9 
     int size=9;
-    sorting_pointers(arr,size);
+    sorting_pointers(arr,size);  // passing the argument in the sorting_pointers!!
     for(int i=0;i<size;i++)
     {
         cout << arr[i] << " ";
