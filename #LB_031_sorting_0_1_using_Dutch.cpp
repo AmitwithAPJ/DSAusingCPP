437ea5bc2b20ac58e2d6d8fbc1aff83ccc9e26c1
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-21 14:37:31 +0530

    Update #LB_031_sorting_0_1_using_Dutch.cpp

--- #LB_031_sorting_0_1_using_Dutch.cpp
+++ #LB_031_sorting_0_1_using_Dutch.cpp
@@ -9,7 +9,7 @@ void sorting_array(int arr[], int size)
     {
         if(arr[left]==0)
         {
-           left++;
+           left++;   //increamenting the value of left!!
         }
         if(arr[left]==1)
         {
@@ -21,7 +21,7 @@ void sorting_array(int arr[], int size)
 int main()
 {
     int arr[]={0,1,1,0,0,1,0,1,0,0,1};
-    int size=11;
+    int size=11;  //size set to be 11 but subject to user's input
     sorting_array(arr,size);
     for(int i=0;i<size;i++)
     {
