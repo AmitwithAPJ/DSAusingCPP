a59a1ae10af8131eb270d62862b65b9b0f1e5472
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-05-25 15:39:44 +0530

    Update #LB_068_Divide_BS_NEG.cpp

--- #LB_068_Divide_BS_NEG.cpp
+++ #LB_068_Divide_BS_NEG.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #include <iomanip>
 double Divide_BS(int Dividend, int Divisor)
 {
-    double start=0;
+    double start=0;  //initializing the start 
     double ans=-1;
     double end=Dividend;
     double precision=0.01;
