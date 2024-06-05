9672155f4601612ff9bdd059efddeae56f243979
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-06-05 19:41:23 +0530

    Update #LB_10_OOPS_macros_P2.cpp

--- #LB_10_OOPS_macros_P2.cpp
+++ #LB_10_OOPS_macros_P2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define MAXX(x,y) (x>y?x:y)
 void func()
 {
-  int x=50;
+  int x=50;  //
   int y=30;
   int result=MAXX(x,y);
   cout << "The Max result is: " << result << endl;
