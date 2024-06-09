0f4c98bf082fafb5479f6e17d99de0f7f6448ddd
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-06-09 13:15:50 +0530

    Update #LB_79_Char_Arr_Basic_QTA.cpp

--- #LB_79_Char_Arr_Basic_QTA.cpp
+++ #LB_79_Char_Arr_Basic_QTA.cpp
@@ -5,9 +5,9 @@ int main()
   char ch[10];
   cout << "Enter the name:";
   cin.getline(ch,25); //entered Amit Ki Jai Ho
-  cout << "Direct Used: " << endl << ch << endl;  //this print whole despite the size is 10.
+  cout << "Direct Used: " << endl << ch << endl;  //this print whole despite the size is 10..
   cout << "Using For!!" << endl;
-  int size=sizeof(ch)/sizeof(ch[0]); //this gives size 10 despite ch took whole Amit ki wala 
+  int size=sizeof(ch)/sizeof(ch[0]); //this gives size 10 despite ch took whole Amit ki wala (BTW i am taking about amit as input)
   //so size uspe depend kare
   for(int i=0;i<size;i++) //however this for itsn't printing it shows stack overflow, why?
   {
