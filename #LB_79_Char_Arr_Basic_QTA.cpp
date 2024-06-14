dd465a88b45c9e3d1eb95012e227786dfe4bde21
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-06-15 03:26:35 +0530

    Update #LB_79_Char_Arr_Basic_QTA.cpp

--- #LB_79_Char_Arr_Basic_QTA.cpp
+++ #LB_79_Char_Arr_Basic_QTA.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
+#include <iostream>  //adding the header the file "iostream"!!
 using namespace std;
 int main()
 {
   char ch[10];
-  cout << "Enter the name:";
+  cout << "Enter The Name: ";
   cin.getline(ch,25); //entered Amit Ki Jai Ho
-  cout << "Direct Used: " << endl << ch << endl;  //this print whole despite the size is 10..
+  cout << "Direct Used: " << endl << ch << endl;  //this print whole despite the size is 10.....
   cout << "Using For!!" << endl;
-  int size=sizeof(ch)/sizeof(ch[0]); //this gives size 10 despite ch took whole Amit ki wala (BTW i am taking about amit as input)
-  //so size uspe depend kare
-  for(int i=0;i<size;i++) //however this for itsn't printing it shows stack overflow, why?
+  int size=sizeof(ch)/sizeof(ch[0]); //this gives size 10 despite ch took whole Amit ki wala (BTW I am taking about amit as input)
+  //so SIZE uspe depend kare
+  for(int i=0;i<size;i++) //However this for itsn't printing it shows stack overflow, why?
   {
     cout << ch[i] <<" ";
   }
-
   return 0;
 }
