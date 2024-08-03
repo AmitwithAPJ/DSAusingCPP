a015d2ed03ad12a7bf0fa698ceaa102422feeac9
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-08-03 13:44:39 +0530

    Update #LB_015_array_reverse.cpp

--- #LB_015_array_reverse.cpp
+++ #LB_015_array_reverse.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main ()
 {
     int arr[7]={2,4,6,8,10,0,12};
-    int size=6;// the size of the array given or you can calculate by your own!!!
+    int size=6;
+    // the size of the array given or you can calculate by your own!!!
     for (int i=0;i<size;i++)
     {
         cout << arr[i] << " ";
