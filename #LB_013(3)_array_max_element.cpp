9467c555c78b246f7986100b267550e04117a42f
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-08-06 14:26:24 +0530

    Update #LB_013(3)_array_max_element.cpp

--- #LB_013(3)_array_max_element.cpp
+++ #LB_013(3)_array_max_element.cpp
@@ -5,7 +5,7 @@ int main ()
 {
     int arr[10]={1,2,3,4,5,-1,100,-100,400,321};
     int size=10;
-    int ans=arr[0];  //storing value in asnwer!!
+    int ans=arr[0];  //storing the value  of 0th index value in an array in "ans" variable!!
     for (int i=1; i<size; i++)
     {
       ans=max(ans,arr[i]);
