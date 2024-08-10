caa308ceb1e93d6a557ddadbb4e6436b982cb1f1
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-08-10 19:49:31 +0530

    Update #LB_014(3)_array_min_element.cpp

--- #LB_014(3)_array_min_element.cpp
+++ #LB_014(3)_array_min_element.cpp
@@ -3,7 +3,8 @@ using namespace std;
 #include <limits.h>
 int main ()
 {
-    int arr[10]={1,2,3,4,5,1,100,100,400,321};  //an array created of size "10"
+    int arr[10]={1,2,3,4,5,1,100,100,400,321};  
+    //an array created of size "10"
     int size=10;
     int ans=INT_MAX;
     for (int i=0; i<size; i++)
